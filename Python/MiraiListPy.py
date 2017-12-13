3241f8570f988fa04c52a3460ab39e77f88bb110
Faruk GÜNGÖR <farcompen71@gmail.com>
2017-12-13 15:51:11 +0300

    Add files via upload

---
+++ Python/MiraiListPy.py
@@ -0,0 +1,56 @@
+#! /usr/bin python3
+# -*- coding:utf-8 -*-
+
+
+
+import  sqlite3
+from datetime import datetime
+
+giris = """
+              *********************************
+             *                                *
+             *        Mirai Botnet            *
+             *       İp Veritabanı            *
+             *                                *
+             *      Faruk GÜNGÖR@2017         *
+             *                                *
+             **********************************
+
+
+
+       """
+print(giris)
+baglanti = sqlite3.connect('MiraiBotnetIpDB.sqlite')
+if baglanti:
+    print ("bağlantı başarılı")
+else:
+    print ("bağlantı başarısız")
+baslangic = datetime.now()
+parametre = """
+              *****************************************************
+              1- Tüm Veritabanı Listele
+              2- Sadece Türkiye İP'leri listele
+              3- İlk 1000 Kayıt Getir
+              4- İP Ara
+
+             ******************************************************
+           """
+
+print(parametre)
+
+secim = input("Seçiminiz : \t")
+dbSecim = baglanti.cursor()
+sorgu = {1:"Select rowid,ASNInfo,IPAddress,DateFirstSeen,Country from mirai ", 2:"Select rowid,ASNInfo,IPAddress,DateFirstSeen,Country from mirai where Country='TR'", 3:"Select rowid,ASNInfo,IPAddress,DateFirstSeen,Country from mirai  LIMIT 1000", }
+
+sorguNo = int(secim)
+kayitGetir = dbSecim.execute(sorgu[sorguNo])
+for veriler in kayitGetir.fetchall():
+    print('Sıra : %d - ASNBilgi: %s - IP:%s- TespitTarihi : %s - Ulke: %s '%veriler)
+    print("")
+baglanti.commit()
+baglanti.close()
+bitis = datetime.now()
+
+total = bitis - baslangic
+
+print ("toplam işlem süresi : {}".format(total))
